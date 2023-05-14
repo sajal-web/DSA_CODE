62745a27600899c82a94797d81fb8af2431d4978
sajal-web <mahatosajal65@gmail.com>
2023-05-14 08:54:19 +0530

    Array Creation

---
+++ ARRAYS/ArrayCreation.java
@@ -0,0 +1,9 @@
+public class ArrayCreation{
+    public static void main(String args[]){
+        int marks[] = new int[50];// array memory allocation
+        // creation of array
+        int numbers[] =  {12,34,45,56}; // creation with memory allocation
+        String fruits[] = {"Mango","Apple","Graps"};
+
+    }
+}
