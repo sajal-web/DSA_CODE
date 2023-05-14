5e9976b3e822917738a1628ae29cd72f4dcb6304
sajal-web <mahatosajal65@gmail.com>
2023-05-14 08:59:36 +0530

    Array I/O

--- ARRAYS/ArrayCreation.java
+++ ARRAYS/ArrayCreation.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class ArrayCreation{
     public static void main(String args[]){
         int marks[] = new int[50];// array memory allocation
@@ -5,5 +6,13 @@ public class ArrayCreation{
         int numbers[] =  {12,34,45,56}; // creation with memory allocation
         String fruits[] = {"Mango","Apple","Graps"};
 
+        // Input / Output in array
+        // 1. Input
+        Scanner sc = new Scanner(System.in);
+        marks[0] = sc.nextInt();
+        marks[1] = sc.nextInt();
+        marks[2] = sc.nextInt();
+        // 2. output 
+        System.out.print(marks[0]+" , "+marks[1]+"  , "+marks[2]);
     }
 }
