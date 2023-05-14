0fe1b6b03a7e251882450d6d0fd1537f6975a9a0
sajal-web <mahatosajal65@gmail.com>
2023-05-14 09:28:46 +0530

    Homewark linear search done

--- ARRAYS/LinearSearch.java
+++ ARRAYS/LinearSearch.java
@@ -3,11 +3,24 @@ public class LinearSearch{
         int numbers[] = {2,4,6,8,10,12};
         int key = 10;
        System.out.print(LinearSearchFunction(numbers,key));
+
+       // Homework
+       String menu[] = {"chola batora","luchi","dim toast","cha","coffee","pokora"};
+       String item = "dim toast";
+       System.out.print(FoundMenuIteam(menu,item));
     }
     static int LinearSearchFunction(int numbers[],int key){
         // Traversing and checking the key 
         for(int i =0;i<numbers.length;i++){
             if(numbers[i]==key){
+                return i;
+            }
+        }
+        return -1;
+    }
+    static int FoundMenuIteam(String menu[],String item){
+        for(int i = 0;i<menu.length;i++){
+            if(menu[i] == item){
                 return i;
             }
         }
