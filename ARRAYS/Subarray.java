43f832d2c8bf43172dac96d86ae72cd867238ce6
sajal-web <mahatosajal65@gmail.com>
2023-05-17 13:35:22 +0530

    Subarray Sum(max)

--- ARRAYS/Subarray.java
+++ ARRAYS/Subarray.java
@@ -4,17 +4,25 @@ public class Subarray{
         printSubArray(numbers);
     }
     static void printSubArray(int numbers[]){
+        int maxSum = Integer.MIN_VALUE;
+        int currentSum = 0;
         // here we are using 3 nested loop to get our subarray 
         // first loop or the outer loop is for geting the start index
         for(int i = 0;i<numbers.length;i++){
-            // secound loop or first inner loop is for getting the ending index
+            
+            // secound loop or fi rst inner loop is for getting the ending index
             for(int j =i;j<numbers.length;j++){
+                currentSum = 0;
                 // third loop to print the subarrays
-                for(int k = i;k<j;k++){
-                    System.out.print(numbers[k]+"  ");
+                for(int k = i;k<=j;k++){
+                    currentSum+=numbers[k];
                 }
-                System.out.println();
+                if(maxSum < currentSum){
+                    maxSum = currentSum;
+                }
+               
             }
         }
+     System.out.print(maxSum);  
     }
 }
